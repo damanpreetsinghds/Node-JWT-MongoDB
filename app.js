c715a0dd1128d0076aad2abf8f1c77adfa8249ef
Damanpreet Singh <daman.preetsingh@yahoo.co.in>
2017-07-06 17:38:13 +0530

    error handling

--- app.js
+++ app.js
@@ -54,11 +54,13 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
+    if (err.name === 'UnauthorizedError') {
+        return res.sendStatus(401);
+    }
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    if (err.status === 401)
-        return res.sendStatus(401);
+
     // render the error page
     res.status(err.status || 500);
     res.render('error');
